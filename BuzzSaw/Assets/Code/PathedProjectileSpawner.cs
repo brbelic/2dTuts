ab22a328f5157efbe61c33cce674e47098c07c18
Branimir Belić <br.belic@gmail.com>
2017-04-12 23:30:07 +0200

    23rd part of the tutorial
    - creating cannon and pathed projectiles

---
+++ BuzzSaw/Assets/Code/PathedProjectileSpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathedProjectileSpawner : MonoBehaviour {
+
+    public Transform Destination;
+    public PathedProjectile Projectile;
+
+    public float Speed;
+    public float FireRate;
+
+    private float _nextShootInSeconds;
+
+    public void Start(){
+
+        _nextShootInSeconds = FireRate;
+    }
+
+    public void Update(){
+
+        if ((_nextShootInSeconds -= Time.deltaTime) > 0)
+            return;
+
+        _nextShootInSeconds = FireRate;
+        var projectile = (PathedProjectile)Instantiate(Projectile, transform.position, transform.rotation);
+        projectile.Initialize(Destination, Speed);
+    }
+
+    public void OnDrawGizmos(){
+
+        if (Destination == null)
+            return;
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, Destination.position);
+    }
+}
