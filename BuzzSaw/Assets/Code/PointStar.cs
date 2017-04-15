cb3c84c59c863d007ba2c59b7c1e9d2cbfc05483
Branimir Belić <br.belic@gmail.com>
2017-04-15 14:49:58 +0200

    33rd part of the tutorial
    - creating simple animations
    -- stars, health packs, cannon firing

--- BuzzSaw/Assets/Code/PointStar.cs
+++ BuzzSaw/Assets/Code/PointStar.cs
@@ -8,8 +8,15 @@ public class PointStar : MonoBehaviour, IPlayerRespawnListener {
     public GameObject Effect;
     public int PointsToAdd = 10;
     public AudioClip HitStarSound;
+    public Animator Animator;
+    public SpriteRenderer Renderer;
+
+    private bool _isCollected;
 
     public void OnTriggerEnter2D(Collider2D other) {
+
+        if (_isCollected)
+            return;
 
         if (other.GetComponent<Player>() == null)
             return;
@@ -20,13 +27,22 @@ public class PointStar : MonoBehaviour, IPlayerRespawnListener {
         GameManager.Instance.AddPoints(PointsToAdd);
         Instantiate(Effect, transform.position, transform.rotation);
 
-        gameObject.SetActive(false);
-
         FloatingText.Show(string.Format("+{0}!", PointsToAdd), "PointStarText", new FromWorldPointTextPositioner(Camera.main, transform.position, 1.5f, 50));
+
+        _isCollected = true;
+
+        Animator.SetTrigger("Collect");
+    }
+
+    public void FinishAnimationEvent() {
+
+        Renderer.enabled = false;
+        Animator.SetTrigger("Reset");
     }
 
     public void OnPlayerRespawnInThisCheckpoint(Checkpoint checkpoint, Player player)
     {
-        gameObject.SetActive(true);
+        _isCollected = false;
+        Renderer.enabled = true;
     }
 }
